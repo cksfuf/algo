77132c6137b93ef5159b110eb8399d6f6327ecbe
cksfuf <s3147078@gmail.com>
2024-07-01 17:23:16 +0900

    update

--- programmers/숨어있는숫자의덧셈1/sol.py
+++ programmers/숨어있는숫자의덧셈1/sol.py
@@ -7,8 +7,13 @@ def solution(my_string):
     return answer
 
 print(solution("aAb1B2cC34oOp"))
-print(solution("1a2b3c4d123"))
+print(solution("1a2b3c4d123"))```
 
+# 주어진 n에는 숫자와 문자가 합쳐진 문자형 데이터
+# 숫자만 합하기 위해 isdigit method 활용
+    # isdigit은 숫자열이 아니라면 False 즉, 0을 반환
+# n에 들어온 데이터 중 문자는 0 값을, 숫자는 숫자형으로 변환되어 answer 공간에 저장되어 더해짐.
+# 결과값 도출.
 
 # 2번
 def solution(my_string):
--- programmers/특정문자제거하기/sol.py
+++ programmers/특정문자제거하기/sol.py
@@ -7,6 +7,12 @@ def solution(my_string, letter):
 print(solution('abcdef', 'f'))
 print(solution('BCBdbe', 'B'))
 
+# 하나의 문자열 데이터 수정하는 method인 replace를 활용
+# 수집된 n이라는 문자열에서 j라는 문자를 공백으로 처리하는 코드 작성.
+# 빈공간 acswer에 수정하는 데이터 할당
+# 할당된 answer 추출하는 함수 작성.
+
+
 # 2. 
 def solution(my_string, letter):
     answer = ''
@@ -14,6 +20,5 @@ def solution(my_string, letter):
         if i != letter:
             answer += i
     return answer
-
 print(solution('abcdef', 'f'))
 print(solution('BCBdbe', 'B'))
